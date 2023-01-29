f3d62fce1548974aeeec60b1c9b072bedc38239c
Carlos Acosta <carlosra0345@gmail.com>
2023-01-29 11:34:53 -0600

    starting to link get requests through express

--- server.js
+++ server.js
@@ -20,12 +20,51 @@ const notesSchema = {
 const Note = mongoose.model("Note", notesSchema);
 
 
-
+//HTTP Protocol requests
 app.get("/", function(req,res){
     //res.send("express is working")
     res.sendFile(__dirname + "/signup.html");
 })
 
+//sending HTTP information for login page
+app.get("/login.html",function(req,res){
+    res.sendFile(__dirname + "/login.html");
+})
+
+//HTTP request for home page
+app.get("/html/home.html",function(req,res){
+    res.sendFile(__dirname + "/html/home.html");
+})
+
+//HTTP request for about page
+app.get("/html/about.html",function(req,res){
+    res.sendFile(__dirname + "/html/about.html");
+})
+
+//HTTP request for courses page
+app.get("/html/courses.html",function(req,res){
+    res.sendFile(__dirname + "/html/courses.html");
+})
+
+//HTTP request for playlist1
+app.get("/html/playlist1.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist1.html");
+})
+app.get("/html/watch-interest1.html",function(req,res){
+    res.sendFile(__dirname + "/html/watch-interest1.html");
+})
+app.get("/html/playlist2.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist2.html");
+})
+app.get("/html/watch-interest2.html",function(req,res){
+    res.sendFile(__dirname + "/html/watch-interest2.html");
+})
+
+
+
+
+
+//database posting to mongoDB cloud server
 app.post("/", function(req,res){
     let newNote = new Note({
         firstName: req.body.firstName,
@@ -40,6 +79,7 @@ app.post("/", function(req,res){
 //linking static files as public to the client
 var path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'html'))); //linking main pages
 
 //app.post request
 app.listen(3000,function(){
