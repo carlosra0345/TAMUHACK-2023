824a1bdbb5558a8763797e7ac1d9ca108f771e92
Carlos Acosta <carlosra0345@gmail.com>
2023-01-29 10:45:59 -0600

    implemented form functionality for mongoDB server

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ mongoose.connect("mongodb+srv://carlosra0345:oNsm9wKksa0BEeet@cluster0.widmf4b.m
 
 //creating data schema
 const notesSchema = {
-    title : String,
-    content : String
+    firstName : String,
+    lastName : String,
+    userEmail : String,
+    userPassword : String
 }
 
 const Note = mongoose.model("Note", notesSchema);
@@ -21,8 +23,23 @@ const Note = mongoose.model("Note", notesSchema);
 
 app.get("/", function(req,res){
     //res.send("express is working")
-    res.sendFile("signup.html");
+    res.sendFile(__dirname + "/signup.html");
 })
+
+app.post("/", function(req,res){
+    let newNote = new Note({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userEmail: req.body.userEmail,
+        userPassword: req.body.userPassword
+    });
+    newNote.save();
+    res.redirect('/'); //redirection after submitting form
+})
+
+//linking static files as public to the client
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
 
 //app.post request
 app.listen(3000,function(){
