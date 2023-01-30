9c2203a53e1401380df1dbf54b28303cf07c68b4
Carlos Acosta <carlosra0345@gmail.com>
2023-01-29 20:32:28 -0600

    finished server post and routes

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ const notesSchema = {
 const Note = mongoose.model("Note", notesSchema);
 
 
-//HTTP Protocol requests
+//HTTP Protocol request for root route(signup.html)
 app.get("/", function(req,res){
     //res.send("express is working")
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/login.html");
 })
 
 //sending HTTP information for login page
-app.get("/login.html",function(req,res){
-    res.sendFile(__dirname + "/login.html");
+app.get("/signup.html",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
 })
 
 //HTTP request for home page
@@ -41,24 +41,59 @@ app.get("/html/about.html",function(req,res){
     res.sendFile(__dirname + "/html/about.html");
 })
 
+//HTTP request for update page
+app.get("/html/update.html",function(req,res){
+    res.sendFile(__dirname + "/html/update.html");
+})
+
 //HTTP request for courses page
 app.get("/html/courses.html",function(req,res){
     res.sendFile(__dirname + "/html/courses.html");
 })
 
-//HTTP request for playlist1
+//HTTP request for profile page
+app.get("/html/profile.html",function(req,res){
+    res.sendFile(__dirname + "/html/profile.html");
+})
+
+//HTTP request for playlists
 app.get("/html/playlist1.html",function(req,res){
     res.sendFile(__dirname + "/html/playlist1.html");
-})
-app.get("/html/watch-interest1.html",function(req,res){
-    res.sendFile(__dirname + "/html/watch-interest1.html");
 })
 app.get("/html/playlist2.html",function(req,res){
     res.sendFile(__dirname + "/html/playlist2.html");
 })
+app.get("/html/playlist3.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist3.html");
+})
+app.get("/html/playlist4.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist4.html");
+})
+app.get("/html/playlist5.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist5.html");
+})
+app.get("/html/playlist6.html",function(req,res){
+    res.sendFile(__dirname + "/html/playlist6.html");
+})
+
+
+//HTTP request for watch-interest playlists
+app.get("/html/watch-interest1.html",function(req,res){
+    res.sendFile(__dirname + "/html/watch-interest1.html");
+})
 app.get("/html/watch-interest2.html",function(req,res){
     res.sendFile(__dirname + "/html/watch-interest2.html");
 })
+app.get("/html/watch-interest3.html",function(req,res){
+    res.sendFile(__dirname + "/html/watch-interest3.html");
+})
+app.get("/html/watch-interest4.html",function(req,res){
+    res.sendFile(__dirname + "/html/watch-interest4.html");
+})
+
+//IF VIDEOS DO NOT WORK, KEEP MAKING APP ROUTES TO VIDEO HTML
+
+
 
 
 
